79e95f480e628fcde5991a430662caf2135c8130
huutri148 <huutri148@gmail.com>
2020-12-02 13:03:24 +0700

    Fix TailAttack

--- SuperMarioBros3/GameObject.cpp
+++ SuperMarioBros3/GameObject.cpp
@@ -129,7 +129,7 @@ void GameObject::RenderBoundingBox()
 	rect.right = (int)r - (int)l;
 	rect.bottom = (int)b - (int)t;
 	Game::GetInstance()->Draw(x, y, bbox, rect.left, rect.top, 
-		rect.right, rect.bottom, 0);
+		rect.right, rect.bottom, 255);
 }
 
 //void GameObject::AddAnimation(int aniId)
--- SuperMarioBros3/Mario.h
+++ SuperMarioBros3/Mario.h
@@ -18,13 +18,15 @@ class Mario : public GameObject
 	DWORD jumpTimeStart = 0;// pressing jumping time
 	DWORD floatingTime = 0;
 	DWORD shootingTime = 0;
-	int powerMelterStack = 0;
+	DWORD tailAttackTime = 0;
 
+	int powerMelterStack = 0;
 	int indexFireBall = 0;
 	int score = 0;
 	int money = 0;
 	int life = 4;
 	int type = 0;
+	int stageOfSwingTail;
 
 	//Các biến cờ ở các trạng thái
 	bool isInGround ;
@@ -34,12 +36,14 @@ class Mario : public GameObject
 	bool canBrake = false;
 	bool isPickingUp = false;
 	
+
 public:
 	// Chuyển thành hàm
 	//vector<FireBall*> fireBall;
 	
 	bool turnFriction;
 	bool isPressedJ;
+	bool isSwingTail = false;
 	bool GetisPickUp() { return isPickingUp; }
 	bool IsFlying();
 	bool IsFloating();
@@ -60,15 +64,21 @@ public:
 	void SetLevel(int l) { form = l; }
 	void SetDirect(bool nx);// Set hướng di chuyển cho mario
 
+
+
 	int GetWidth();
 	int GetHeight();
 	void Information();
 	int GetForm() { return form; };
+	int GetMoney() { return money; };
+	int GetScore() { return score; };
+	int GetLife() { return life; };
+	int GetPowerMelter() { return powerMelterStack; };
 
 	//Xử Lí
 	void Friction();
-	void HandleCollision(float min_tx, float min_ty,
-		float nx, float ny, float x0, float y0);
+	/*void HandleCollision(float min_tx, float min_ty,
+		float nx, float ny, float x0, float y0);*/
 	void StartUntouchable() 
 	{ untouchable = 1; untouchableStart = GetTickCount(); }
 	void UpForm();
@@ -95,14 +105,12 @@ public:
 	void SuperJump();
 	void PickUp();
 	bool Brake();
+	void UpdateStageOfTailAttack();
 
 	void GainMoney(int pMoney) { money += pMoney; };
 	void GainPoint(int pPoint) { score += pPoint; };
 	void GainLife() { life += 1; };
 
-	int GetMoney() { return money; };
-	int GetScore() { return score; };
-	int GetLife() { return life; };
-	int GetPowerMelter() { return powerMelterStack; };
+
 
 };
